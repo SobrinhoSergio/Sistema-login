32c45e48dce57d5f0de35ddbc73de5c000bb10d2
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2020-08-24 16:16:40 -0300

    iniciado o projeto Front-end

---
+++ cadastrar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Sistema de Login</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+<a id="voltar" href="index.php">
+    <img src="imagens/icons/back.svg" alt="voltar">
+</a>
+
+<div id="corpo-form-cad">
+    <h1>Cadastrar</h1>
+
+    <form action="processa.php" method="POST">
+        <input type="text" id="" name="" placeholder="Nome Completo"/>
+        <input type="text" id="" name="" placeholder="Telefone"/>
+        <input type="email" id="" name="" placeholder="Usuário"/>
+        <input type="password" id="" name="" placeholder="Senha"/>
+        <input type="password" id="" name="" placeholder="Confirmar Senha"/>
+        <input type="submit" value="CADASTRAR" id="" name=""/>
+    </form>
+<div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <title>Sistema de Login</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+<div id="corpo-form">
+    <h1>Entrar</h1>
+
+    <form action="processa.php" method="POST">
+        <input type="email" id="" name="" placeholder="Usuário"/>
+        <input type="password" id="" name="" placeholder="Senha"/>
+        <input type="submit" value="ACESSAR" id="" name=""/>
+        <a href="cadastrar.php">Ainda não é inscrito? <strong>Inscreva-se!</strong></a>
+    </form>
+<div>
+</body>
+</html>
