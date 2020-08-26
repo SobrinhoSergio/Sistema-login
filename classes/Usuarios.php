a454f8638a2bcbf1ab0b02baac43da64786f2a29
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2020-08-26 16:13:10 -0300

    Inserindo dados no BD

--- classes/Usuarios.php
+++ classes/Usuarios.php
@@ -11,7 +11,7 @@ class Usuarios{
         global $msgERRO;
 
         try{
-            $pdo = new PDO("mysql:bdname=".$nome.";host=".$host,$usuario,$senha);
+            $pdo = new PDO("mysql:dbname=".$nome.";host=".$host,$usuario,$senha);
 
         }
         catch (PDOException $e){
@@ -21,31 +21,52 @@ class Usuarios{
     }
 
     public function cadastrar($nome, $telefone, $email, $senha){
+        
         global $pdo;
         $sql = $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e");
-        $sql = bindValue(":e", $email);
-        $sql = execute();
+        $sql-> bindValue(":e", $email);
+        $sql-> execute();
 
         if ($sql->rowCount()>0):
             
-            return false; else:
+            return false; 
+        
+        else:
 
-            $sql = $pdo->prepare("INSERT INTO usuarios (nome, telefone, email, senha) VALUES (:n, :t, :e, :s)");
-        $sql = bindValue(":n", $nome);
-        $sql = bindValue(":t", $telefone);
-        $sql = bindValue(":e", $email);
-        $sql = bindValue(":s", $senha);
+        $sql = $pdo->prepare("INSERT INTO usuarios (nome, telefone, email, senha) VALUES (:n, :t, :e, :s)");
+        $sql-> bindValue(":n", $nome);
+        $sql-> bindValue(":t", $telefone);
+        $sql-> bindValue(":e", $email);
+        $sql-> bindValue(":s", md5($senha));
 
         $sql->execute();
         return true;
 
         endif;
     }
-
+    
     public function logar($email, $senha){
 
         global $pdo;
 
+        $sql= $pdo->prepare("SELECT id_usuario FROM usuarios WHERE email = :e AND senha = :s");
+
+        $sql = bindValue(":e", $email);
+        $sql = bindValue(":s", md5($senha));
+        $sql = execute();
+
+        if($sql->rowCount()>0):
+            $dado = $sql->fetch();
+            session_start();
+            $_SESSION['id_usuario'] = $dado['id_usuario'];
+            return true; 
+
+        else:
+            return false;
+
+        endif;
 
     }
 }
+
+?>
